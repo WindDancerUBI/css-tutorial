16a8ab8540a3236053453c671675e7b957e17e0e
huangjitao <164173925@qq.com>
2023-04-15 12:53:09 +0800

    feat: 响应式布局

---
+++ 8-responsive/responsive.js
@@ -0,0 +1,6 @@
+var menuButton = document.getElementById('toggle-menu');
+menuButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  var menu = document.getElementById('main-menu');
+  menu.classList.toggle('is-open');
+});
